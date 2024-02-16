d8367d77603e25ba2dd6b11f2afb6bd00f3a7d94
chc1129 <chc1129@gmail.com>
2024-02-17 06:11:17 +0900

    code fix

---
+++ 023-GuessTheWordGame/words-db.js
@@ -0,0 +1,33 @@
+let wordList = [
+    {
+        word: "banana",
+        hint: "yellow fruit"
+    },
+    {
+        word: "candy",
+        hint: "weet treat"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        
+    }
+]
