6e184ca450b3a3fe638bd86ccf9e5fdd93ef7038
chc1129 <chc1129@users.noreply.github.com>
2024-02-24 11:28:48 +0900

    Update index.js

--- 025-JSCharts/index.js
+++ 025-JSCharts/index.js
@@ -40,4 +40,3 @@ const chartOptions = {
 // Create new ApexCharts instance with chart options and render it
 const chart = new ApexCharts(document.querySelector('.chart-area'), chartOptions);
 chart.render();
-
