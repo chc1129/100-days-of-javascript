7fc03afcfca60abbca5c5a03bdd25b2564246252
chc1129 <chc1129@gmail.com>
2024-03-02 12:55:55 +0900

    code fix

--- 026-RecipeGuideApp/index.js
+++ 026-RecipeGuideApp/index.js
@@ -1,12 +1,12 @@
 const resultContainer = document.getElementById("result");
-const searchBtn = document.getElementById("serach-button");
-const serachInput = document.getElementById("serach-input");
-const searchContainer = document.querySelector(".serach-box");
+const searchBtn = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const searchContainer = document.querySelector(".search-box");
 
 // Api url to fetch meal data
 const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-// Event listeners for serach and input (when press enter)
+// Event listeners for search and input (when press enter)
 searchBtn.addEventListener("click", searchMeal);
 searchInput.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {
@@ -19,7 +19,7 @@ searchInput.addEventListener("keydown", function (e) {
 function searchMeal() {
     const userInput = searchInput.value.trim();
     if (!userInput) {
-        resultContainer.innerHTML = `<h3>Input Field Cannot be Empty</h3>`;
+        resultContainer.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
         return;
     }
     // Fetch meal data using api with user input
@@ -27,18 +27,18 @@ function searchMeal() {
         const meal = data.meals[0];
         // Handle where no meal found
         if (!meal) {
-            resultContainer.innerHTML = `<h3>No Meal Found, Please Try Again</h3>`;
+            resultContainer.innerHTML = `<h3>No Meal Found, Please Try Again!</h3>`;
             return;
         }
         const ingredients = getIngredients(meal);
-        // Generate Html to dipslay meal data
+        // Generate Html to display meal data
         const recipeHtml = `
             <div class="details">
                 <h2>${meal.strMeal}</h2>
                 <h4>${meal.strArea}</h4>
             </div>
             <img src=${meal.strMealThumb} alt=${meal.strMeal} />
-            <div id="ingre-contailner">
+            <div id="ingre-container">
                 <h3>Ingredients:</h3>
                 <ul>${ingredients}</ul>
             </div>
@@ -54,14 +54,14 @@ function searchMeal() {
         hideRecipeBtn.addEventListener("click", hideRecipe);
         const showRecipeBtn = document.getElementById("show-recipe");
         showRecipeBtn.addEventListener("click", showRecipe);
-        searchContainer.computedStyleMap.opacity = '0';
-        searchContainer.computedStyleMap.display = 'none';
+        searchContainer.style.opacity = '0';
+        searchContainer.style.display = 'none';
     })
         // Handle error
         .catch(() => {
             searchContainer.style.opacity = '1';
             searchContainer.style.display = 'grid';
-            resultContainer.innerHTML = '<h3>Erro fetching data!</h3>'
+            resultContainer.innerHTML = `<h3>Error fetching data!</h3>`;
         });
 }
 
