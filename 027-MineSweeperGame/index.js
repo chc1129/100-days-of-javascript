3c51628c57ebec61163c06030df2ad7096a8dec5
chc1129 <chc1129@gmail.com>
2024-03-02 13:45:16 +0900

    code fix

---
+++ 027-MineSweeperGame/index.js
@@ -0,0 +1,10 @@
+const grid = document.getElementById("grid");
+let lockGame = false;
+// Set test mode to true if you want see mines location
+const testMode = false;
+generateGrid();
+
+// Generate 10 * 10 Grid
+function generateGrid() {
+    
+}
