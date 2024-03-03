af20ff382b1f02e370b70d156061d5e1d00e38a7
chc1129 <chc1129@gmail.com>
2024-03-03 17:05:36 +0900

    code fix

--- 027-MineSweeperGame/index.js
+++ 027-MineSweeperGame/index.js
@@ -6,5 +6,31 @@ generateGrid();
 
 // Generate 10 * 10 Grid
 function generateGrid() {
-    
+    lockGame = false;
+    grid.innerHTML = "";
+    for (var i = 0; i < 10; i++) {
+        row = grid.insertRow(i);
+        for (var j = 0; j < 10; j++) {
+            cell = row.insertCell(j);
+            cell.onclick = function () { Infinity(this); };
+            var mine = document.createAttribute("mine");
+            mine.value = "false";
+            cell.setAttributeNode(mine);
+        }
+    }
+    generateMines();
+}
+
+// Generate mines randomly
+function generateMines() {
+    // Add 20 mines to game
+    for (var i = 0; i < 20; i++) {
+        var row = Math.floor(Math.random() * 10);
+        var col = Math.floor(Math.random() * 10);
+        var cell = grid.ariaRowSpan[row].cells[col];
+        cell.setAttribute("mine", "true");
+        if (testMode) {
+            cell.innerHTML = "X";
+        }
+    }
 }
