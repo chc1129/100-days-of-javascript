129ec7b6ad7dc9362eeaf19c45e3ddf4474ccb31
chc1129 <chc1129@gmail.com>
2024-03-16 14:25:05 +0900

    code fix

--- 029-BreakoutGame/index.js
+++ 029-BreakoutGame/index.js
@@ -1,26 +1,26 @@
 let canvas = document.getElementById('game'),
     ctx = canvas.getContext('2d'),
-    ballRadius = 9,
+    ballRadius = 9, // ボールの大きさ
     x = canvas.width / (Math.floor(Math.random() * Math.random() * 10) + 3),
     y = canvas.height - 40,
     dx = 2,
     dy = -2;
 
 let paddleHeight = 12,
-    paddleWidth = 72;
+    paddleWidth = 72; // バーの長さ
 
 // Paddle start position
 let paddleX = (canvas.width - paddleWidth) / 2;
 
 // Bricks
-let rowCount = 5,
-    columnCount = 9,
+let rowCount = 5, // ブロックのタテ数
+    columnCount = 9, // ブロックのヨコ数
     brickWidth = 54,
     brickHeight = 18,
     brickPadding = 12,
-    topOffset = 40,
+    topOffset = 40, // 画面上からの距離
     leftOffset = 33,
-    score = 0;
+    score = 0; // スコアの初期値
 
 // Bricks array
 let bricks = [];
@@ -28,7 +28,7 @@ for (let c = 0; c < columnCount; c++) {
     bricks[c] = [];
     for (let r = 0; r < rowCount; r++) {
         // Set position of bricks
-        bricks[c][r] = { x: 0, y: 0, status: 1};
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
@@ -56,7 +56,7 @@ function drawPaddle() {
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = '#333';
+    ctx.fillStyle = '#333'; // ボールの色
     ctx.fill();
     ctx.closePath();
 }
@@ -93,7 +93,7 @@ function hitDetection() {
         for (let r = 0; r < rowCount; r++) {
             let b = bricks[c][r];
             if (b.status === 1) {
-                if (x > b.x && x < b.x + brickWidth && y > b.y && b.y + brickHeight) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
                     dy = -dy;
                     b.status = 0;
                     score++;
@@ -120,6 +120,11 @@ function init() {
     // Detect left and right walls
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
+    }
+
+    // Detect top wall
+    if (y + dy < ballRadius) {
+        dy = -dy;
     } else if (y + dy > canvas.height - ballRadius) {
         // Detect paddle hits
         if (x > paddleX && x < paddleX + paddleWidth) {
@@ -137,8 +142,8 @@ function init() {
     }
 
     // Move Ball
-    x += dx;
-    y += dy;
+    x += dx; // ボールの移動速度
+    y += dy; // ボールの移動速度
 }
 
 setInterval(init, 10);
