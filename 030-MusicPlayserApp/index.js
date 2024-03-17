748122cd936db9022cbf2818372da9bfc09b921f
chc1129 <chc1129@gmail.com>
2024-03-17 12:23:04 +0900

    code fix

--- 030-MusicPlayserApp/index.js
+++ 030-MusicPlayserApp/index.js
@@ -71,7 +71,32 @@ function loadMusic() {
 }
 
 function changMusic(direction) {
-    musicIndex = (musicIndex + direction + song.length) % song.length;
+    musicIndex = (musicIndex + direction + song.length) % songs.length;
     loadMusic(song[musicIndex]);
     playMusic();
 }
+
+function updateProgressBar() {
+    const { duration, currentTime } = music;
+    const progressPerrcent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) => String(math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changMusic(-1));
+nextBtn.addEventListener('click', () => changMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
