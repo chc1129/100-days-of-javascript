3f8ba40085fe0d2daab0bdc650f4f4a33d7f12ba
chc1129 <chc1129@gmail.com>
2024-03-20 22:49:41 +0900

    code fix

--- 031-ChatGPTBot/index.js
+++ 031-ChatGPTBot/index.js
@@ -1,1 +1,27 @@
- 
+const charLog = document.getElementById('chat-log'),
+    userInput = document.getElementById('user-input'),
+    sendButton = document.getElementById('send-button'),
+    buttonIcon = document.getElementById('button-icon'),
+    info = document.querySelector('.info');
+
+sendButton.addEventListener('click', sendMessage);
+userInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+function sendMessage() {
+    const message = userInput.value.trim();
+    // if message = empty do nothing
+    if (message === '') {
+        return;
+    }
+    // if message = developer - show our message
+    else if (message === 'developer') {
+        // clear input value
+        userInput.value = '';
+        // append message as user - we will code it's function
+        appendMessage('user', message);
+    }
+}
