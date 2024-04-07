47a984f78fc46b9cb8017f41b417facffebd3a81
chc1129 <chc1129@gmail.com>
2024-04-08 01:24:59 +0900

    code fix

---
+++ 032-GoogleMapsApi/index.js
@@ -0,0 +1,19 @@
+// Set map
+function initialize() {
+    var mapOptions = {
+        // Zoom of map on start
+        zoom: 10,
+        // Initial center cordinates on start (NewYork)
+        center: new google.maps.LatLng(40.7128, -74.0060),
+        // Type of map (ROADMAP, SATELLITE, HYBRID, TERRAIN)
+        mapTypeID: google.Map.mapTypeID.RADMAP,
+        // Minimum zoom of map
+        minZoom: 2
+    };
+
+    // Create a new map instance using provided options
+    var map = new google.map.Map(document.getElementById('map'), mapOptions);
+
+    // Create an info window to display location info
+    var infoWindow = new google.maps.infoWindow();
+}
