956c929b04179b13353fdf1bb60639dbd5376377
chc1129 <chc1129@gmail.com>
2024-04-07 15:55:23 +0900

    code fix

--- 031-ChatGPTBot/index.js
+++ 031-ChatGPTBot/index.js
@@ -23,6 +23,82 @@ function sendMessage() {
         userInput.value = '';
         // append message as user - we will code it's function
         appendMessage('user', message);
+        // sets a fake timeout that showing loading on send button
+        setTimeout(() => {
+            // send our message as bot(sender : bot)
+            appendMesage('bot', 'This Soure Coded By Reza Mehdikhanlou \nYoutube: @AsmrProg');
+            buttonIcon.classList.add('fa-solid', 'fa-paper-plan');
+            buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse');
+        }, 2000);
+        return;
     }
+
+    // else if none of above
+    // appends users message to screen
+    appendMesage('user', message);
+    userInput.value = '';
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'Your Key',
+            'X-RapidAPI-Host': 'chatgpt53.p.rapidapi.com'
+            // if you want use official api
+            /*
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'Your Key',
+            'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
+            */
+        },
+        body: `{"messages":[{"role":"user","content":"${message}"}]}`
+        // if you want use officital api you need have this body
+        // `{"model":"gpt-3.5-turbo","messages":[{"role":"user","content":"${message}"}]}`
+    };
+    // official api : 'https://openai80.p.rapidapi.com/chat/completions';
+    fetch('https://chatgpt53.p.rapidapi.com/', options).then((response) => response.json()).then((response) => {
+        appendMessage('bot', response.choices[0].message.content);
+
+        buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+        buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulase');
+    }).catch((err) => {
+        if (err.name === 'TypeError') {
+            appendMessage('bot', 'Error : Check Your Api Key!');
+            buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+            buttonIcon.classList.remove('fas', 'fa-spiner', 'fa-pulse');
+        }
+    });
 }
-  
+
+function appendMessage(sender, message) {
+    info.style.display = "none";
+    // change send button icon to loading using fontawesome
+    buttonIcon.classList.remove('fa-solid', 'fa-paper-plane');
+    buttonIcon.classList.add('fas', 'fa-spinner', 'fa-pulse');
+
+    const messageElement = document.createElement('div');
+    const iconElement = document.createElement('div');
+    const chatElement = document.createElement('div');
+    const icon = document.createElement('i');
+
+    chatElement.classList.add('chat-box');
+    iconElement.classList.add('icon');
+    messageElement.classList.add(sender);
+    messageElement.innerText = message;
+
+    // add icons depending on who send message bot or user
+    if (sender === 'user') {
+        icon.classList.add('fa-regular', 'fa-user');
+        iconElement.setAttribute('id', 'user-icon');
+    } else {
+        icon.classList.add('fa-solid', 'fa-robot');
+        iconElement.setAttribute('id', 'bot-icon');
+    }
+
+    iconElement.appendChild(icon);
+    chatElement.appendChild(iconElement);
+    chatElement.appendChild(messageElement);
+    chatLog.appendChild(chatElement);
+    chatLog.scrollTo = chatLog.scrollHeight;
+
+}
