5f2c47ebf6e00a44c738b880402371bed27f100b
chc1129 <chc1129@gmail.com>
2024-04-12 00:59:29 +0900

    code fix

--- 032-GoogleMapsApi/index.js
+++ 032-GoogleMapsApi/index.js
@@ -16,11 +16,30 @@ function initialize() {
 
     // Create an info window to display location info
     var infoWindow = new google.maps.infoWindow();
+
+    // Create a marker for example : Iran, Zanjan
+    var marker = new google.maps.Marker({
+        // Cordinates for Iran, Zanjan
+        position: new google.maps.LatLng(36.6769, 48.4964),
+        // Attach the marker
+        map: map,
+        // Tooltip on hover
+        title: 'Iran, Zanjan'
+    });
+
+    // Add click event listener for marker
+    marker.addListener('click', function () {
+        infoWindow.setContent(marker.title);
+        infoWindow.open(map, marker);
+    });
+
+    // Adjust map center when the window is resized
+    google.maps.event.addDomListener(window, "resize", function () {
+        map.setCenter(mapOptions.center);
+    });
+
+
 }
 
-
-
-
-
-
-
+// Initialize the map when window loading finished
+google.maps.event.addDomListener(window, 'load', initialize);
