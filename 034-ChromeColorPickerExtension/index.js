c688492f13cafcc7d8e8971ff23b6857e593fe16
chc1129 <chc1129@gmail.com>
2024-04-27 21:27:10 +0900

    code fix

--- 034-ChromeColorPickerExtension/index.js
+++ 034-ChromeColorPickerExtension/index.js
@@ -1,4 +1,10 @@
+const pickerBtn = document.querySelector("#picker-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const colorList = document.querySelector(".all-colors");
+const exportBtn = document.querySelector("#export-btn");
 
+// Retrieving picked colors from localstorage or initializing an empty array
+let pickedColors = JSON.parse(localStorage.getItem("colors-list")) || [];
 
-
-
+// Variable to keep track of the current color popup
+let currentPopup = null;
